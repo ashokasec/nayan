1893196a8cbb25853d953187338adfde1320d470
Shivam Gupta <ashokasec@proton.me>
2024-07-26 15:23:02 +0530

    add navbar

---
+++ src/_libs/fonts.tsx
@@ -0,0 +1,6 @@
+import { Bricolage_Grotesque, Open_Sans, Playfair_Display, Inter } from 'next/font/google'
+
+export const bricolage = Bricolage_Grotesque({ subsets: ["latin"] });
+export const open_sans = Open_Sans({ subsets: ["latin"] });
+export const inter = Inter({ subsets: ["latin"] });
+export const playfair_display = Playfair_Display({ subsets: ["latin"] });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Navbar from "@/_components/app/navbar";
+import { inter } from "@/_libs/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
